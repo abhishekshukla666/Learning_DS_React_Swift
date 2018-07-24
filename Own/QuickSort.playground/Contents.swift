bf328880c0aea12930d3a26a8988890c9da7746e
Abhishek Shukla <t-abhishek2.shukla@ocltp.com>
2018-07-24 14:13:37 +0530

    LinkedList file renamed

--- Own/QuickSort.playground/Contents.swift
+++ Own/QuickSort.playground/Contents.swift
@@ -13,42 +13,8 @@ import Foundation
 //    return quicksort(less) + equal + quicksort(greater)
 //}
 //
-//// Method 2
-//func quicksortLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) {
-//    if low < high {
-//        let p = partitionLomuto(&a, low: low, high: high)
-//        print("1st Lomuto - Low: \(low), high: \(p-1)")
-//        quicksortLomuto(&a, low: low, high: p - 1)
-//        print("2nd Lomuto - Low: \(p+1), high: \(high)")
-//        quicksortLomuto(&a, low: p + 1, high: high)
-//    }
-//}
-//
-//func partitionLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) -> Int {
-//    let pivot = a[high] // Making Hight value as pivot
-////    print("Pivot: \(pivot)")
-//
-//    var i = low
-//    for j in low..<high {  // Starting loop from low to high
-////        print("i: \(i)")
-////        print("J: \(j)")
-////        print("Comparing a[j]:\(a[j]) and pivot: \(pivot)")
-//        if a[j] <= pivot {
-////            print("swaping a[i]: \(a[i]), a[j]: \(a[j])")
-//            a.swapAt(i, j)
-////            (a[i], a[j]) = (a[j], a[i])
-//            i += 1
-//        }
-//    }
-////    (a[i], a[high]) = (a[high], a[i])
-//    a.swapAt(i, high)
-//
-////    print("Returning i:\(i)")
-////    print("\n")
-//    return i
-//}
 
-
+// Method 2
 func quickSort<T: Comparable>(_ array: inout [T], low: Int, high: Int, isOrdered: (T, T) -> Bool) -> [T] {
     if low < high {
         let i = partition(&array, low: low, high: high, isOrdered: isOrdered)
