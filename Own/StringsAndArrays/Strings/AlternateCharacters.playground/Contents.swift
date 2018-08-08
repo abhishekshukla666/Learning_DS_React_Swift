7596466ab0dfce9ba96ba11b2ce6ec75f90ed59d
Abhishek Shukla <t-abhishek2.shukla@ocltp.com>
2018-08-08 12:42:17 +0530

    Added some files

--- Own/StringsAndArrays/Strings/AlternateCharacters.playground/Contents.swift
+++ Own/StringsAndArrays/Strings/AlternateCharacters.playground/Contents.swift
@@ -1,5 +1,17 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/*
+ - The characters marked red are the ones that can be deleted so that the string doesn't have matching consecutive characters.
+ */
 
-var str = "Hello, playground"
+var str = "AAAA"
+
+var dict = [String: Int]()
+
+for ch in str {
+    print(ch)
+}
+
+
+
+
+
