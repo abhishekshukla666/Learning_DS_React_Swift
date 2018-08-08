1fece0d4174239f6a5fb0c7673b542fac4dbcfeb
Abhishek Shukla <t-abhishek2.shukla@ocltp.com>
2018-08-08 11:28:04 +0530

    Anagram String Problem solved

--- Own/StringsAndArrays/AnagramsString.playground/Contents.swift
+++ Own/StringsAndArrays/AnagramsString.playground/Contents.swift
@@ -4,20 +4,41 @@
  - Making an String Anagram
  */
 
-extension String {
-    subscript (i: Int) -> Character {
-        return self[index(startIndex, offsetBy: i)]
+let str1 = "jxwtrhvujlmrpdoqbisbwhmgpmeoke"
+let str2 = "fcrxzwscanmligyxyvym"
+
+var dict1 = [String: Int]()
+var dict2 = [String: Int]()
+var count = 0
+
+for ch in str1.lowercased() {
+    let s = String(describing: ch)
+    if dict1[s] != nil {
+        count = dict1[s]!
+        dict1[s] = count + 1
+    } else {
+        dict1[s] = 1
     }
 }
 
-var str1 = "abc"
-var str2 = "cde"
-
-for i in 0..<str1.count {
-    for j in 0..<str2.count {
-        let ch1 = str1[i]
-        let ch2 = str2[j]
-        print("Ch1: \(ch1) \t Ch2: \(ch2)\n")
+for ch in str2.lowercased() {
+    let s = String(describing: ch)
+    if dict2[s] != nil {
+        count = dict2[s]!
+        dict2[s] = count + 1
+    } else {
+        dict2[s] = 1
     }
 }
+
+var charCountToRemove = 0
+for ch in "abcdefghijklmnopqrstuvwxyz" {
+    let ch = String(describing: ch)    
+    var firstCount = 0
+    var secondCount = 0
+    if let firstChCount = dict1[ch] { firstCount = firstChCount } else { firstCount = 0 }
+    if let secondChCount = dict2[ch] { secondCount = secondChCount } else { secondCount = 0 }
+    charCountToRemove += abs(firstCount - secondCount)
+}
+print("Remove Character count: ", charCountToRemove)
 
