a07ad6c4b93b15ef5dbf55e2df1e1ffd1b110d2b
Abhishek Shukla <t-abhishek2.shukla@ocltp.com>
2018-08-21 12:35:57 +0530

    Any and AnyObject

--- Own/Swift/AnyAndAnyObject.playground/Contents.swift
+++ Own/Swift/AnyAndAnyObject.playground/Contents.swift
@@ -1,5 +1,8 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
 
 var str = "Hello, playground"
+
+var arr: [AnyObject] = [str as AnyObject, "Abhishek" as AnyObject, 1 as AnyObject]
+var arr2: [Any] = [1, str, true]
+print(arr2)
