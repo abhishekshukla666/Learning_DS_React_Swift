874d780768064801e42f39f02a2a517c23320d23
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Pro.local>
2018-09-11 09:33:10 +0530

    Dependency Inversion Principle Problem and solution implemented

--- Own/Swift/DIProblem.playground/Contents.swift
+++ Own/Swift/DIProblem.playground/Contents.swift
@@ -1,7 +1,15 @@
 
 /*
  - Why is Inversion of Control required?
- - Here is the problem
+ 
+ The problem here is tight coupling in classes. In other words, "class A depends on class B". Let's take an example and try to understand the tight coupling.
+ 
+ Suppose we have a Samsung class that contains an Android class object. An issue with this is tight coupling between classes. In other words the Samsung class depends on the Android object. So if the Android class changes for any reason then it may affect the Samsung class.
+ 
+ The problem is that the Samsung class controls the creation of the Android object.
+ The Android class is directly referenced in the Samsung class that leads to tight coupling among address and customer objects.
+ If we make any changes to the Android class then it might affect the Samsung class also, because the Samsung class is dependent on the Android class. Suppose that we add some properties or methods to the Android class then it might require changes in the Samsung class.
+ In easier words, this is called Object Dependency. Object Dependency means that, for one object  to work, it needs another object. In other words one object is dependent on another object.
  */
 
 class Android {
