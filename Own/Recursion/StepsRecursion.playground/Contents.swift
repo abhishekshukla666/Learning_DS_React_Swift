e3b3a5aa17a1a181393e356b1e15051a0907e640
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Pro.local>
2018-10-07 08:24:02 +0530

    David's StairCase using Bottom Up Approach

--- Own/Recursion/StepsRecursion.playground/Contents.swift
+++ Own/Recursion/StepsRecursion.playground/Contents.swift
@@ -4,11 +4,46 @@
  - using Recursion
  */
 
-var numberOfWays = 0
+
+
 func numbWays(n: Int) -> Int {
 
-    if n == 0 || n == 1 { return 1 }
-    return numbWays(n: n - 1) + numbWays(n: n - 2) + 1
+    if n < 2 { return 1 }
+    else if n == 2 { return 2 }
+    return numbWays(n: n - 1) + numbWays(n: n - 2) + numbWays(n: n - 3)
 }
 
-print("Number of ways to reach to : \(numbWays(n: 3)) floor")
+var numberOfWays = 0
+func numbWays(dp n: Int, memo: [Int]) -> Int {
+    
+    if memo.indices.contains(n) { return memo[n] }
+    if n == 0 || n == 1 { return 1 }
+    else if n == 2 { return 2 }
+    return numbWays(dp: n - 1, memo: memo) + numbWays(dp: n - 2, memo: memo) + numbWays(dp: n - 3, memo: memo)
+}
+
+func numbWays(bottomUp n: Int, memo: [Int]) -> Int {
+    
+    var memo = memo
+    if n == 0 {
+        return 1
+    } else if n == 1 {
+        return 1
+    } else if n == 2 {
+        return 2
+    }
+    memo.insert(1, at: 0)
+    memo.insert(1, at: 1)
+    memo.insert(2, at: 2)
+    
+    for i in 3...n {
+        memo.insert(memo[i-1] + memo[i-2] + memo[i-3], at: i)
+    }
+    return memo[n]
+}
+
+
+var memo = [Int]()
+var floor = 7
+//print("Number of ways to reach to reach \(floor)th floor: \(numbWays(bottomUp: floor, memo: memo))")
+print("Number of ways to reach to reach \(floor)th floor: \(numbWays(dp: floor, memo: memo))")
